175611f9e7f0bffb23e2f081284d4ae7193fec8f
abmf001-gits <abmf001@gmail.com>
2026-08-02 19:34:33 +0530

    Add Randomizer class for question randomization
    Implemented Randomizer class for assessment simulation.

---
+++ js/randomizer.js
@@ -0,0 +1,82 @@
+/**
+ * ==========================================
+ * MedPlus Pharmacist Assessment Simulator
+ * Randomizer Engine v1.0
+ * ==========================================
+ */
+
+class Randomizer {
+
+    constructor(questions = []) {
+
+        this.questionBank = questions;
+
+    }
+
+    setQuestionBank(questions) {
+
+        this.questionBank = questions;
+
+    }
+
+    shuffle(array) {
+
+        const arr = [...array];
+
+        for (let i = arr.length - 1; i > 0; i--) {
+
+            const j = Math.floor(Math.random() * (i + 1));
+
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+
+        }
+
+        return arr;
+
+    }
+
+    shuffleOptions(question) {
+
+        const shuffledOptions = this.shuffle(question.options);
+
+        return {
+
+            ...question,
+
+            options: shuffledOptions
+
+        };
+
+    }
+
+    generateAssessment(totalQuestions = 50) {
+
+        if (!this.questionBank.length) {
+
+            console.error("Question Bank Empty");
+
+            return [];
+
+        }
+
+        const shuffledQuestions = this.shuffle(this.questionBank);
+
+        const selectedQuestions = shuffledQuestions.slice(
+
+            0,
+
+            Math.min(totalQuestions, shuffledQuestions.length)
+
+        );
+
+        return selectedQuestions.map(question =>
+
+            this.shuffleOptions(question)
+
+        );
+
+    }
+
+}
+
+const randomizer = new Randomizer();
